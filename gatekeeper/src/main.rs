35b6491951ddc99afabbfc3df81c758ae04690ac
Kiwis <69465940+KiwisDev@users.noreply.github.com>
2026-05-18 16:58:56 -0400

    Working gatekeeper - first draft

--- gatekeeper/src/main.rs
+++ gatekeeper/src/main.rs
@@ -14,4 +14,17 @@ async fn main() {
     let redis_url = std::env::var("REDIS_URL").unwrap_or_else(|_| "redis://127.0.0.1:6379".to_string());
 
     let pool = redis_pool::create_pool(&redis_url).expect("Error when creating Redis pool");
+
+    let shared_state = Arc::new(AppState {redis_pool: pool});
+
+    let app = Router::new()
+        .route("/health", get(handlers::health_handler))
+        .route("/login", post(handlers::login_handler))
+        .with_state(shared_state);
+
+    let addr = "0.0.0.0:3000";
+    let listener = TcpListener::bind(addr).await.unwrap();
+    println!("Gatekeeper listening on {}", addr);
+
+    axum::serve(listener, app).await.unwrap();
 }
